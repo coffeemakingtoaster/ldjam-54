89d433dfbae24ae8a352d947315fae9266bf5bba
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-09-30 13:54:38 +0200

    feat(traintrack): add basic traintrack system

---
+++ Assets/Scripts/TrainLocomotive.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class TrainLocomotive : MonoBehaviour
+{
+    public TrainTrack nextTrainTrack;
+    public float SPEED = 5f;
+
+    bool isInTrackTile = false;
+
+    float rotatedAngle = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(nextTrainTrack.entryPoint.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = this.GetNextWaypoint();
+
+        // Has traintrack center been reached?
+        if (Vector3.Distance(targetPosition, transform.position) < 0.02 || rotatedAngle >= 90f)
+        {
+            // Has reached entrypoint => Target exitpoint of same track
+            // Has reched exitpoint => Target entrypoint of next track
+            if (!isInTrackTile)
+            {
+                isInTrackTile = true;
+            }
+            else
+            {
+                TrainTrack nextPossibleTrack = nextTrainTrack.GetNextTrainTrack();
+                Debug.Log("Find next");
+                // Is next defined?
+                if (nextPossibleTrack != null)
+                {
+                    nextTrainTrack = nextPossibleTrack;
+                    isInTrackTile = false;
+                    rotatedAngle = 0f;
+                }
+            }
+        }
+        else
+        {
+            if (isInTrackTile && nextTrainTrack.isCurve)
+            {
+                transform.RotateAround(nextTrainTrack.rotatePoint.transform.position, Vector3.up, -50f * Time.deltaTime);
+                rotatedAngle += Mathf.Abs(-10f * Time.deltaTime);
+            }
+            else
+            {
+                Vector3 lookVector = (targetPosition - transform.position).normalized;
+                Quaternion toRotation = Quaternion.LookRotation(lookVector);
+                transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, Time.deltaTime);
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, SPEED * Time.deltaTime);
+            }
+        }
+    }
+
+    Vector3 GetNextWaypoint()
+    {
+        if (isInTrackTile)
+        {
+            Debug.Log("Target exitpoint");
+            return nextTrainTrack.exitPoint.transform.position;
+        }
+        Debug.Log("Target entrypoint");
+        return nextTrainTrack.entryPoint.transform.position;
+    }
+}

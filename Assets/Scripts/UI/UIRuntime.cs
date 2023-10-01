fc6d066f968e5471b24a8e4d2fd168157a90c215
Freezerino <lkleinau@icloud.com>
2023-10-01 15:52:28 +0200

    Add UI Base

--- Assets/Scripts/UI/UIRuntime.cs
+++ Assets/Scripts/UI/UIRuntime.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class UIRuntime : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button buttonPlay = root.Q<Button>("ButtonStart");
+
+        buttonPlay.clicked += () => SceneManager.LoadScene("GameScene", LoadSceneMode.Additive);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

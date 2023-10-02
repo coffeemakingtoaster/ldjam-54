f32dd800e028a5f91bfdeb9c3ed811e468f2f8f4
Freezerino <lkleinau@icloud.com>
2023-10-02 03:27:08 +0200

    Tooltip

--- Assets/Scripts/UI/UIGameRuntime.cs
+++ Assets/Scripts/UI/UIGameRuntime.cs
@@ -10,39 +10,60 @@ public class UIGameRuntime : MonoBehaviour
     private VisualElement root;
     private VisualElement gameUI;
     private VisualElement menuUI;
+    private VisualElement trackToolTip;
     private Button buttonSettings;
+    private Button buttonCuttingBoard;
+    private Button buttonPlate;
+    private Button buttonPan;
+    private Button buttonPot;
+    private Button buttonFry;
+    private Button buttonMeat;
+    private Button buttonExtra;
+    private Button trackStraight;
+    private Button trackTurn;
+    private Button trackSwitch;
+    private Label cash;
     private VisualElement buttonWrapper;
     private GameObject PlacementSystem;
     private void Awake()
     {
+        // -- INITIALIZATION --
         root = GetComponent<UIDocument>().rootVisualElement;
 
-        //root.Q<Button>("ButtonStart");
+        buttonCuttingBoard = root.Q<Button>("CuttingBoard");
+        buttonPlate = root.Q<Button>("Plate");
+        trackTurn = root.Q<Button>("TrackTurn");
 
-        Button buttonSchneidebrett = root.Q<Button>("Schneidebrett");
+        
+        
+        trackToolTip = root.Q<VisualElement>("TrackTooltip");
         Button buttonExit = root.Q<Button>("ButtonExit");
         buttonSettings = root.Q<Button>("ButtonSettings");
         buttonWrapper = root.Q<VisualElement>("MenuBar");
         //gameUI = root.Q<VisualElement>("MenuBar");
         Button buttonHowTo = root.Q<Button>("ButtonHowTo");
 
-        buttonSchneidebrett.clicked += () => ps.StartPlacement(2);
+        //buttonSchneidebrett.clicked += () => ps.StartPlacement(2);
         buttonExit.clicked += ExitApplicationClicked;
-        buttonSettings.clicked += () => ps.StartPlacement(0);
+        trackTurn.clicked += TrackTurnClicked;
+        buttonPlate.clicked += () => Debug.Log(buttonPlate);
+        //buttonSettings.clicked += () => ps.StartPlacement(0);
 
 
         //buttonSettings.clicked += SettingsClicked;
-    }
-
-    private void PlayGame()
-    {
-        SceneManager.LoadScene("MainMenu");
     }
     
     private void SettingsClicked()
     {
         buttonWrapper.Clear();
     }
+
+    private void TrackTurnClicked()
+    {
+        trackToolTip.visible = true;
+        Debug.Log("TrackTurn");
+    }
+
 
     private void ExitApplicationClicked()
     {
